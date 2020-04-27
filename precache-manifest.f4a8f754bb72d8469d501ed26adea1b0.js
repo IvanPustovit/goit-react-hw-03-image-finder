813010cc7c5522e5e30c0340d1bd73441940d0b3
IvanPustovit <pustovit.for@gmail.com>
2020-04-27 20:07:49 +0300

    Updates

---
+++ precache-manifest.f4a8f754bb72d8469d501ed26adea1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6838e5bb5027a84521ed3663ee96bc87",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9617db3365ee1eddd435",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.f28b99d8.chunk.css"
+  },
+  {
+    "revision": "941a23342159fcf6b5db",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.dee30841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.dee30841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9617db3365ee1eddd435",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.edc7f906.chunk.js"
+  },
+  {
+    "revision": "5c2a2e40467f9c888b88",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.abfcbc42.js"
+  }
+]);
